3e7bcbc675e40b05b3a5e94f05799b5bf80c3569
raashidbaba <raashidbaba849@gmail.com>
2023-01-30 15:54:47 +0530

    added share option.

--- app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameWonFragment.kt
@@ -1,9 +1,8 @@
 package com.example.androidtrivia
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -23,11 +22,50 @@ class GameWonFragment : Fragment() {
             false
         )
         binding.nextMatchButton.setOnClickListener {
-           // it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment2)
-            it.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment2())
+            // it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment2)
+            it.findNavController()
+                .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment2())
         }
         val args = GameWonFragmentArgs.fromBundle(arguments)
-        Toast.makeText(context,"Num correct: ${args.numCorrect},Num Questions : ${args.numQuestions}",Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            context,
+            "Num correct: ${args.numCorrect},Num Questions : ${args.numQuestions}",
+            Toast.LENGTH_LONG
+        ).show()
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    // Creating our Share Intent
+    private fun getShareIntent(): Intent {
+        var args = GameWonFragmentArgs.fromBundle(arguments)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+            )
+        return shareIntent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
